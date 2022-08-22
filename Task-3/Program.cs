e194158b3281c755dde47f963037aa4357e57a3c
Vladimir Avdeyev <avd-vlad@yandex.ru>
2022-08-22 21:44:27 +0300

    Initial commit

---
+++ Task-3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*---------------------------------------------------------------------------------------
+Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+---------------------------------------------------------------------------------------*/
+
+bool CheckNumber(int x, int a, int b)
+{
+    if((x%a==0)&(x%b)==0)
+        return true;
+    //else return false;
+    return false;
+}
+
+Console.WriteLine("Программа проверки кратности числа двум другим числам");
+
+Console.Write("Введите число для проверки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите первое условие кратности: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+
+Console.Write("Введите второе условие кратности: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+bool result = CheckNumber(x, a, b);
+
+if (result)
+    Console.WriteLine($"Число {x} кратно числу {a} и числу {b}");
+else
+    Console.WriteLine($"Число {x} не кратно числам {a} и {b}");
---
+++ Task-4/Program.cs
@@ -0,0 +1,36 @@
+﻿/*------------------------------------------------------------------------------------------------------------
+Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+------------------------------------------------------------------------------------------------------------*/
+
+bool CheckNumbers(int a, int b)
+{
+    if(a<b)
+    {
+        if(a*a==b)
+            return true;
+    }
+    else
+    {
+        if(b*b==a)
+            return true;
+    }
+    return false;
+}
+
+Console.WriteLine("Прорамма проверки отношения квадрата между числами");
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+bool res = CheckNumbers(a,b);
+
+if(res)
+    Console.WriteLine($"Числа {a} и {b} удовлетворяют условиям задачи");
+else
+    Console.WriteLine($"Числа {a} и {b} не удовлетворяют условиям задачи");
+   
